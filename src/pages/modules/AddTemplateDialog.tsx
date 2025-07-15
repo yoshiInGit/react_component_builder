404e294e0a8e7d370f856a6637ecd9825afc55c4
yoshiInGit <yoshimain016@gmail.com>
2025-07-15 11:02:20 +0900

    ダイアログを修正

--- src/pages/modules/AddTemplateDialog.tsx
+++ src/pages/modules/AddTemplateDialog.tsx
@@ -77,7 +77,7 @@ const AddTemplateDialog: React.FC<Props> = ({ isOpen, onClose, onSubmit }) => {
                 type="submit"
                 className="cursor-pointer w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
               >
-                送信
+                追加
               </button>
               <button
                 type="button"
